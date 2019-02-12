1cf7279fc49623082791e79d70c86f1fc19e254d
so.sorphorn <so.sorphorn@gmail.com>
2019-02-12 13:54:44 +0700

    Done feature add facebook button in login

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
-import './Reankour/login_page.dart';
 import './Reankour/auth.dart';
 import './Reankour/root_page.dart';
 
 void main() {
 	runApp(
 		MaterialApp(
-			// title: "Interest Calculate App",
-      title: "NoteKeeper",
+      title: "ReanKour",
       debugShowCheckedModeBanner: false,
-			// home: FavoriteCity(),
-      // home: InterestCalculator(),
-      // theme: ThemeData(
-      //   brightness: Brightness.dark,
-      //   primaryColor: Colors.indigo,
-      //   accentColor: Colors.indigoAccent,
-      // ), //Calculator interest
-      theme: ThemeData(
-      //  primarySwatch: Colors.deepPurple, 
+      theme: ThemeData( 
       primarySwatch: Colors.blue,
       ),
-      // home: LoginPage(auth: new Auth()),
       home: RootPage(auth: new Auth()),
-      // home: LoginPage(),
 		)
 	);
 }
